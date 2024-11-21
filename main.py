0e3bf172c0ea537f4d6a55a2e8465026819e046a
weirdmaneightmybrain <107234539+weirdmaneightmybrain@users.noreply.github.com>
2024-11-20 17:13:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-string = input()
+string = input("Input:")
 print(string)
 word = ""
 list = []
 newlist = []
-subnumber = -22
+subnumber = -22 # Setting variables, newlist is subtracted bu subnumber
 
-for x in string:
-  if x.isnumeric():
-    word += x
-  else:
-    list.append(word)
-    word = ""
+for x in string: # checks if input is a number, if it is add to word
+    if x.isnumeric():
+        word += x
+    else: # if it aint, add word to list and reset
+        list.append(word)
+        word = ""
 
-for i in list:
-  newint = int(i)
-  newint -= subnumber
-  newlist.append(newint)
-  word += str(chr(newint))
+for i in list: #
+    newint = int(i)
+    newint -= subnumber
+    newlist.append(newint)
+    word += str(chr(newint))
 print(newlist)
 print(word)
